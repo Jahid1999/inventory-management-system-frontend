414b8b08a99b56dab9036bf92eef2f707fedde2a
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-07-17 22:49:31 +0600

    login and logout working

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,13 +9,12 @@ export class AuthService {
 
     constructor(private formbuilder: FormBuilder, private http: HttpClient) { } 
 
-    public authUrl = "http://localhost:52684/api/login/authenticate";
+    public authUrl = "http://localhost:5000/api/login/authenticate";
 
     public adminSignInOperation() {
         var body = {
             "email": this.adminSignInformModel.value.email,
             "password": this.adminSignInformModel.value.password,
-            "role": this.adminSignInformModel.value.role,
         }
 
         return this.http.post<any>(this.authUrl , body);
@@ -24,7 +23,6 @@ export class AuthService {
     adminSignInformModel = this.formbuilder.group({
         email: ['', Validators.required],
         password: ['', Validators.required],
-        role: ['', Validators.required],
     });
 
 }
